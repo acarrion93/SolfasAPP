9e34c2757d6ab86815f2ff18bc5ab7077bc34724
Antonio <acarrionmartinez93@gmail.com>
2020-02-07 15:57:32 +0100

    Commit 9

--- app/src/main/java/com/example/SolfasAPP/LoginActivity.java
+++ app/src/main/java/com/example/SolfasAPP/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.SolfasAPP;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +22,11 @@ public class LoginActivity extends AppCompatActivity {
     private Button mButtonLogin;
     private Button mButtonForgetPass;
     private Button mButtonRegister;
+
+    private String email = "";
+    private String password = "";
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,8 @@ public class LoginActivity extends AppCompatActivity {
         mButtonForgetPass = findViewById(R.id.buttonRecContra);
         mButtonRegister = findViewById(R.id.buttonRegistrar);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,5 +49,35 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = mEmail.getText().toString();
+                password = mPass.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty()){
+                    loginUsuario();
+
+                }else{
+                    Toast.makeText(LoginActivity.this, "Debes de rellenar todos los campos", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
+
+        public void loginUsuario(){
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                        finish();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "No se ha podido entrar a la cuenta del usuario", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
 }
---
+++ app/src/main/java/com/example/SolfasAPP/ProfileActivity.java
@@ -0,0 +1,35 @@
+package com.example.SolfasAPP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Button mButtonSignOut;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        mButtonSignOut = findViewById(R.id.buttonSignOut);
+
+        mButtonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+
+}
